4675d6a23dbbacee36fb7985b822486c57ab17da
OZhang <zhangrunze@163.com>
2017-08-20 22:27:23 +0800

    api/Account/Register
    worked

--- AuthService.js
+++ AuthService.js
@@ -16,46 +16,83 @@ class AuthService {
         })
     }
     login(creds, cb){
-        const b = new buffer.Buffer(creds.username +
-            ':' + creds.password);
-        const encodedAuth = b.toString('base64');
-        console.log(b.toString('base64'));
-        fetch('https://api.github.com/user',{
-            headers:{
-                'Authorization' : 'Basic ' + encodedAuth
-            }
-        })
-            .then((response) =>{
-                if (response.status >= 200 && response.status < 300){
-                    return response;
-                }
+        // const b = new buffer.Buffer(creds.username +
+        //     ':' + creds.password);
+        // const encodedAuth = b.toString('base64');
 
-                throw {
-                    badCredentials: response.status === 401,
-                    unknownError: response.status !== 401
-                }
+
+
+        // console.log(b.toString('base64'));
+        // fetch('https://api.github.com/user',{
+        //     headers:{
+        //         'Authorization' : 'Basic ' + encodedAuth
+        //     }
+        // })
+
+        fetch('http://192.168.1.116/ccwwebapi/api/Account/Register', {
+            method: 'POST',
+            headers: {
+                "Accept": "application/json",
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({
+                "Email": creds.username,
+                "Password": creds.password,
+                "ConfirmPassword": creds.password
             })
-            .then((response) => {
-                return response.json();
-            })
-            .then((results) => {
-            AsyncStorage.multiSet([
-                [authKey, encodedAuth],
-                [userKey, JSON.stringify(results)],
-                    (err) => {
-                    if (err){
-                        throw err;
+            }).then((response) =>{
+                    if (response.status >= 200 && response.status < 300){
+                        return response;
                     }
+
+                    throw {
+                        badCredentials: response.status === 401,
+                        unknownError: response.status !== 401
                     }
-            ]);
-                return cb({success: true});
-            })
-            .catch((err) =>{
-                return cb(err);
-            })
-            .finally(() =>{
-                return cb({showProgress:false});
-            });
+                })
+            // .then((response) => {
+            //     return response.json();
+            // })
+        .then((results) => {
+                    return cb({success: true});
+                })
+                .catch((err) =>{
+                    return cb(err);
+                })
+                .finally(() => {
+                    return cb({showProgress: false});
+                });
+        // }).then((response) =>{
+        //         if (response.status >= 200 && response.status < 300){
+        //             return response;
+        //         }
+        //
+        //         throw {
+        //             badCredentials: response.status === 401,
+        //             unknownError: response.status !== 401
+        //         }
+        //     })
+        //     .then((response) => {
+        //         return response.json();
+        //     })
+        //     .then((results) => {
+        //     AsyncStorage.multiSet([
+        //         [authKey, encodedAuth],
+        //         [userKey, JSON.stringify(results)],
+        //             (err) => {
+        //             if (err){
+        //                 throw err;
+        //             }
+        //             }
+        //     ]);
+        //         return cb({success: true});
+        //     })
+        //     .catch((err) =>{
+        //         return cb(err);
+        //     })
+        //     .finally(() =>{
+        //         return cb({showProgress:false});
+        //     });
     }
 }
 
